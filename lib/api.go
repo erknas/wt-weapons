f7e12891bbf81a9781a0e06f5bd3c42eaf5588d4
Aleksandr <vin89441@gmail.com>
2025-01-25 18:22:03 +0300

    api helpers

---
+++ lib/api.go
@@ -0,0 +1,22 @@
+package lib
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type APIFunc func(w http.ResponseWriter, r *http.Request) error
+
+func MakeHTTPFunc(fn APIFunc) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := fn(w, r); err != nil {
+			WriteJSON(w, http.StatusBadRequest, map[string]any{"error": err.Error()})
+		}
+	}
+}
+
+func WriteJSON(w http.ResponseWriter, status int, v any) error {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	return json.NewEncoder(w).Encode(v)
+}
